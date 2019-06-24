670cacb3c27d50feb8c521ccddbcfd8cc35ee02e
Andres <alexjar22@hotmail.com>
2019-06-23 23:17:07 -0500

    El color del minutero tambien hay que modificiar
    Signed-off-by: Andres <alexjar22@hotmail.com>

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,34 @@
 $(document).ready(function (e) {
 	let intervalTime = 1000;
-	let minutos = 29;
+	let minutos = 19;
 	let display;
-	var timer = minutos, minutes, seconds;
+	var timer = minutos,
+		minutes, seconds;
 	let clock;
+	let nombre;
+	let warning = true;
 
 
 	$(".start-game").click(function (e) {
 		changeBackground();
-		$(this).css({ 'display': 'none' })
+		$(".preguntas-random, .titulo-app, .nombre input").addClass('hidden');
+		$(".pregunta").removeClass('hidden');
+		$(".respuestas").css({
+			'visibility': 'visible'
+		});
+		$(".container-respuestas").css({
+			'margin-top': '9%'
+		});
+		nombre = $("#txtNombre").val();
+		$(".container-puntuacion").css({
+			'color': '#fff'
+		})
+
+		$(this).css({
+			'display': 'none'
+		});
 		$(".container-timer").fadeIn();
+
 		display = $(".time");
 
 		timer = minutos, minutes, seconds;
@@ -27,10 +46,71 @@ $(document).ready(function (e) {
 
 		timer--;
 
-		if (minutes == 0 && seconds == 0) clearInterval(clock);
+		if (minutes == 0 && seconds == 0) {
+			$("#timer").removeClass('danger-clock');
+			clearInterval(clock);
+			return;
+		}
+
+		if (seconds <= 15) {
+			if (seconds <= 5) {
+				$(".timer").addClass('danger-clock');
+				$(".timer").removeClass('warning-clock');
+			} else {
+				$(".timer").removeClass('danger-clock');
+				$(".timer").addClass('warning-clock');
+
+				// setTimeout(function (e) {
+				// 	if (warning) {
+				// 		$('.fill-background').css({
+				// 			'background-color': '#dc0d32'
+				// 		});
+				// 	} else {
+				// 		$('.fill-background').css({
+				// 			'background-color': '#1bb287'
+				// 		});
+
+				// 	}
+				// 	warning = !warning;
+				// }, 1000);
+
+				if(warning){
+					
+					$( ".fill-background" ).animate({
+						backgroundColor: "#dc0d32"
+					}, 400 );
+
+					// $('.timer').css({
+					// 	'--color':"#dc0d32"
+					// });
+
+					// document.querySelectorAll('.timer')[0].style.setProperty("--color", "#dc0d32");
+					// $('.timer').css('data-content','#dc0d32');
+
+
+				}else{
+					$( ".fill-background" ).animate({
+						backgroundColor: "#1bb287"
+					}, 400);
+
+					// document.querySelectorAll('.timer')[0].style.setProperty("--color", "#1bb287");
+					// $('.timer').attr('data-content','#1bb287');
+
+
+				}
+				warning = !warning;
+
+			}
+		} else {
+			$(".timer").removeClass('warning-clock');
+		}
 	}
 
-	function changeBackground(){
+	function changeBackground() {
 		$(".fill-background").addClass('scale-background');
+	}
+
+	function temblarClock() {
+
 	}
 })
